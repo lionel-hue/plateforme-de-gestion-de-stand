c8cee510550491397ad07f9ae7a326bfa7be1ec4
bouabre225 <sissolionel@gmail.com>
2025-07-20 15:19:07 +0200

    modification des routes

--- routes/web.php
+++ routes/web.php
@@ -11,19 +11,19 @@ Route::get('/', [AcceuilController::class, 'index'])->name('acceuil');
 
 //Auth Admin
 Route::prefix('admin')->group(function () {
-    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [AdminAuthController::class, 'login']);
-    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
 });
 
 
 
 //routes Admin
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
-    Route::get('/demandes', [AdminController::class, 'demandes'])->name('admin.demandes');
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/demandes', [AdminController::class, 'demandes'])->name('demandes');
     Route::post('/demandes/{id}/approuver', [AdminController::class, 'approuver']);
     Route::post('/demandes/{id}/rejetter', [AdminController::class, 'rejetter']);
-    Route::get('/stands', [AdminController::class, 'stands'])->name('admin.stands');
+    Route::get('/stands', [AdminController::class, 'stands'])->name('stands');
     Route::get('/commandes/{id}', [AdminController::class, 'commandesParStand']);
 });
