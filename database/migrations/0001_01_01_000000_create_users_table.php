da3b5a7511660d7b06defa750c81e48c1cd739c0
bouabre225 <sissolionel@gmail.com>
2025-07-20 03:21:47 +0200

    mise a jour de la table users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom_entreprise');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'entrepreneur_en_attente', 'entrepreneur_approuve', 'entrepreneur_refuse'])->default('entrepreneur_en_attente');
+            $table->string('status')->default('en_attente');
+            $table->text('raison_rejet')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
