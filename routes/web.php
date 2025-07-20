cddab8556e86ea40fbac71e038d60624436dfe71
bouabre225 <sissolionel@gmail.com>
2025-07-20 03:22:33 +0200

    modification par ajout de la route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AcceuilController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AcceuilController::class, 'index'])->name('acceuil');
