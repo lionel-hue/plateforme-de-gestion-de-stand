fc700c009dbe45c1a99261009dbed9d12b52ff6f
bouabre225 <sissolionel@gmail.com>
2025-07-23 23:44:34 +0200

    modif

--- database/migrations/2025_07_21_123026_create_stands_table.php
+++ database/migrations/2025_07_21_123026_create_stands_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-                Schema::create('stands', function (Blueprint $table) {
+            Schema::create('stands', function (Blueprint $table) {
             $table->id();
             $table->string('nom_stand');
             $table->string('description');
