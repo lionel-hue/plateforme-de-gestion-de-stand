1d89831afb42b3354971571e5eb60ca5c68896d0
bouabre225 <sissolionel@gmail.com>
2025-10-16 14:20:26 +0100

    modif

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\AdminController;
 Route::get('/', [VisiteurController::class, 'index'])->name('accueil');
 
 Route::get('/stand', [VisiteurController::class, 'stand'])->name('stand');
+Route::get('/profile',  [VisiteurController::class, 'profile'])->name('profile');
 
 Route::get('/register/entrepreneur', [EntrepreneurAuthController::class, 'showRegistrationForm'])
     ->name('entrepreneur.register');
@@ -21,8 +22,21 @@ Route::get('/login/entrepreneur', [EntrepreneurAuthController::class, 'showLogin
     ->name('entrepreneur.login');
 Route::post('/login/entrepreneur', [EntrepreneurAuthController::class, 'login'])->name('entre.login');
 
-Route::get('/dashboard/entrepreneur', [EntrepreneurDashboardController::class, 'dashboard'])
-    ->name('entrepreneur.dashboard');
+// Route protege Entrepreneur
+Route::middleware(['auth:entrepreneur'])->prefix('entrepreneur')->group(function () {
+    Route::get('/dashboard', [EntrepreneurDashboardController::class, 'dashboard'])
+    ->name('dashboard');
+    Route::get('/statistique', [EntrepreneurDashboardController::class, 'statistique'])
+    ->name('statistique');
+    Route::get('/orders', [EntrepreneurDashboardController::class, 'orders'])
+    ->name('orders');
+    Route::get('/products', [EntrepreneurDashboardController::class, 'products'])
+    ->name('products');
+    Route::get('/addProduct', [EntrepreneurDashboardController::class, 'addProduct'])
+    ->name('addProduct');
+    Route::post('/logout', [EntrepreneurDashboardController::class, 'logout'])->name('logout');
+});
+
 
 Route::get('/visiteur/register', [VisiteurAuthController::class, 'showRegisterForm'])->name('visiteur.register');
 Route::post('/visiteur/register', [VisiteurAuthController::class, 'register'])->name('visiteur.register');
@@ -39,35 +53,31 @@ Route::get('/evènement', [StandController::class, 'evènement'])->name('evènem
 
 ///Auth Admin
 Route::prefix('admin')->group(function () {
-    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('login');
+    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
     Route::post('/login', [AdminAuthController::class, 'login'])->name('login');
-    Route::get('/register', [AdminAuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AdminAuthController::class, 'register'])->name('register');   
-    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
+});
+
+//routes protege Admin
+Route::middleware(['auth:admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard/{id}', [AdminController::class, 'dashboard'])->name('dashboard');
+    Route::get('/demandes', [AdminController::class, 'demandes'])->name('demandes');
+    Route::post('/demandes/{id}/approuver', [AdminController::class, 'approuver'])->name('approuver');
+    Route::post('/demandes/{id}/rejetter', [AdminController::class, 'rejetter']);
+    Route::get('/stands', [AdminController::class, 'stands'])->name('stands');
+    Route::get('/commandes/{id}', [AdminController::class, 'commandesParStand'])->name('commandes');
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
 });
 
 
 
-//routes Admin
-Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
+/*//routes Admin
+Route::middleware(['auth:admin','admin'])->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
     Route::get('/demandes', [AdminController::class, 'demandes'])->name('demandes');
     Route::post('/demandes/{id}/approuver', [AdminController::class, 'approuver']);
     Route::post('/demandes/{id}/rejetter', [AdminController::class, 'rejetter']);
     Route::get('/stands', [AdminController::class, 'stands'])->name('stands');
     Route::get('/commandes/{id}', [AdminController::class, 'commandesParStand'])->name('commandes');
-});
-
-
-
-//routes Admin
-Route::middleware(['auth','admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
-    Route::get('/demandes', [AdminController::class, 'demandes'])->name('demandes');
-    Route::post('/demandes/{id}/approuver', [AdminController::class, 'approuver']);
-    Route::post('/demandes/{id}/rejetter', [AdminController::class, 'rejetter']);
-    Route::get('/stands', [AdminController::class, 'stands'])->name('stands');
-    Route::get('/commandes/{id}', [AdminController::class, 'commandesParStand'])->name('commandes');
-});
+});*/
 
 
