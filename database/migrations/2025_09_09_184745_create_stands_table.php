7b49e27a37c40d9feb3b1ab091f78c2fd0610355
bouabre225 <sissolionel@gmail.com>
2025-10-16 14:20:07 +0100

    ajout des tables

---
+++ database/migrations/2025_09_09_184745_create_stands_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stands', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('nom_stand');
+            $table->string('description');
+            
+            $table->string('type_stand');
+            $table->string('localisation');
+            $table->string('prix');
+            $table->string('image')->nullable();
+            $table->enum('status', ['disponible', 'reserve', 'non_disponible'])->default('disponible');
+            $table->foreignId('entrepreneur_id')->constrained('entrepreneurs')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stands');
+    }
+};
