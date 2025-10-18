9bc82478713c30fa1b2e55678773a3e30a44adbd
bouabre225 <sissolionel@gmail.com>
2025-10-18 09:20:57 +0100

    modif

--- resources/views/components/aut-layout.blade.php
+++ resources/views/components/aut-layout.blade.php
@@ -4,6 +4,8 @@
     <title>@yield('title') | Eat&Drink</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/admin-style.css') }}">
     <link rel="stylesheet" href="#">
 
